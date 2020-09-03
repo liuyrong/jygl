c6d712aebf7e597a277d0d8485bb52b338d52ea4
liuyuerong <3299275798@qq.com>
2020-09-03 10:12:22 +0800

    后台管理

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+// element-ui注册
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// console.log(process.)
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
-
+// vant组件引入
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
 new Vue({
   router,
   store,
